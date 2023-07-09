b083df5ea889940d644a4b31ae46c87e5f690653
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-08 17:59:58 -0700

    Update Maximum-triplet-sum.js

--- Maximum-triplet-sum.js
+++ Maximum-triplet-sum.js
@@ -36,3 +36,9 @@ function maxTriSum(numbers) {
 }
 
 // or
+
+function maxTriSum(numbers) {
+  numbers.sort((a, b) => b - a)
+  let arr = numbers.filter((e, i) => numbers.indexOf(e) === i)
+  return arr[0] + arr[1] + arr[2]
+}
